7af320f5d250c8b3cc56135038d15a042768b8d3
tyler ferre <ferre.tyler1@gmail.com>
2023-04-07 12:59:46 -0600

    added navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Navbar = () =>{
+    return(
+        <div className="Navbar">
+            <h2>Meme Generator</h2>
+        </div>
+    )
+}
+
+export default Navbar
